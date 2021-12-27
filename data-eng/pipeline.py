319099e24c4a9e03e60d6adfc6ca23638c402388
Daniel Yates <danielyates1234@gmail.com>
2021-12-27 13:44:24 +0000

    updated beam pipeline

--- data-eng/pipeline.py
+++ data-eng/pipeline.py
@@ -10,25 +10,21 @@ with open("./data-eng/netflow_table_schema.json", "r") as f:
 
 table_spec = f"{os.environ['GCP_PROJECT']}:lanl_netflow.netflow_V2"
 
-
-def run():
-    with beam.Pipeline() as pipeline:
-      readable_files = (
-          pipeline
-          | fileio.MatchFiles(f"{os.environ['GCP_BUCKET_NAME']}/compressed/netflow_day-*.bz2")
-          | fileio.ReadMatches())
-      files_and_contents = (
-          readable_files
-          | beam.Map(lambda x: (x.metadata.path, x.read_utf8())))
-      write_files = (
-          files_and_contents
-          | beam.io.WriteToBigQuery(
-                table_spec,
-                schema=table_schema,
-                write_disposition=beam.io.BigQueryDisposition.WRITE_EMPTY,
-                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-                custom_gcs_temp_location=f"gs://{os.environ['GCP_BUCKET_NAME']}/tmp/"
-            ))
-            
-if __name__ == "__main__":
-    run()
+with beam.Pipeline() as pipeline:
+  readable_files = (
+      pipeline
+      | fileio.MatchFiles(f"{os.environ['GCP_BUCKET_NAME']}/compressed/netflow_day-*.bz2")
+      | fileio.ReadMatches())
+  files_and_contents = (
+      readable_files
+      | beam.Map(lambda x: (x.metadata.path, x.read_utf8())))
+  write_files = (
+      files_and_contents
+      | beam.io.WriteToBigQuery(
+            table_spec,
+            schema=table_schema,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_EMPTY,
+            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+            custom_gcs_temp_location=f"gs://{os.environ['GCP_BUCKET_NAME']}/tmp/"
+        ))
+        

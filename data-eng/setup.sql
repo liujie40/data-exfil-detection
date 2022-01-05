24f6cafff2de4476e86fd3b03c89ed279c6bb6a0
Daniel Yates <danielyates1234@gmail.com>
2022-01-05 04:38:18 +0000

    updated sql setup script

--- data-eng/setup.sql
+++ data-eng/setup.sql
@@ -2,8 +2,17 @@
 -- Stratify by number of communications --
 ------------------------------------------
 
-SELECT DISTINCT SrcDevice
-FROM `data-exfil-detection.lanl-netflow.netflow`
-UNION ALL
-SELECT DISTINCT DstDevice
-FROM `data-exfil-detection.lanl-netflow.netflow`
+WITH device_frequencies AS (
+  SELECT device, count(1) as freq
+  FROM (
+    SELECT DISTINCT SrcDevice as device
+    FROM `data-exfil-detection.lanl-netflow.netflow`
+    UNION ALL
+    SELECT DISTINCT DstDevice as device
+    FROM `data-exfil-detection.lanl-netflow.netflow`
+  ) t1
+  GROUP BY device
+  ORDER BY freq DESC
+)
+
+

4582b8c2624eda285a503050ee9d59f852e54af5
Daniel Yates <danielyates1234@gmail.com>
2022-01-05 04:56:13 +0000

    created device frequencies table

--- data-eng/setup.sql
+++ data-eng/setup.sql
@@ -1,18 +1,44 @@
+----------------------------------------------------------------------
+-- TODO:                                                            --
+--     * Check bigquery types and update table creation if required --
+--     * Finish device strata query                                 --
+----------------------------------------------------------------------
+
 ------------------------------------------
 -- Stratify by number of communications --
 ------------------------------------------
 
-WITH device_frequencies AS (
-  SELECT device, count(1) as freq
-  FROM (
-    SELECT DISTINCT SrcDevice as device
-    FROM `data-exfil-detection.lanl-netflow.netflow`
-    UNION ALL
-    SELECT DISTINCT DstDevice as device
-    FROM `data-exfil-detection.lanl-netflow.netflow`
-  ) t1
-  GROUP BY device
-  ORDER BY freq DESC
+CREATE TEMPORARY TABLE _device_frequencies (
+  device STRING,
+  freq INTEGER
 )
 
+CREATE TEMPORARY TABLE _device_strata (
+  device STRING,
+  strata SMALLINT
+)
 
+INSERT INTO _device_frequencies
+SELECT device, COUNT(1) as freq
+FROM (
+  SELECT SrcDevice as device
+  FROM `data-exfil-detection.lanl-netflow.netflow`
+  UNION ALL
+  SELECT DstDevice as device
+  FROM `data-exfil-detection.lanl-netflow.netflow`
+) t1
+GROUP BY device
+ORDER BY freq DESC
+
+
+
+SELECT
+  device,
+  SUM(proportions)
+    OVER (
+      ORDER BY proportions DESC
+    ) AS cum_proportions
+FROM (
+  SELECT device, freq / SUM(freq) AS proportions
+  FROM device_frequencies
+)
